92a2643c79970cf1cb78381f3f9fff60009cf91f
Felipe Almeida <felipepereira937@gmail.com>
2016-06-07 22:59:43 -0300

    Configurações iniciais

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace api_sintegra\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
---
+++ app/sintegra.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace api_sintegra;
+
+use Illuminate\Database\Eloquent\Model;
+
+class sintegra extends Model
+{
+    //
+}
